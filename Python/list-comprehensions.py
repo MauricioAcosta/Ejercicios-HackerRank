310ed19fcfb0ad030b8147ed47da2cd80c50991d
MauricioAcosta <andres.taamap@gmail.com>
2017-04-10 17:06:14 -0500

    problema de cubo convinaciones

--- Python/list-comprehensions.py
+++ Python/list-comprehensions.py
@@ -3,12 +3,4 @@ if __name__ == '__main__':
     Y = int(input())
     Z = int(input())
     N = int(input())
-    combinaciones=[(i,j,k)for i in [X,Y-1,Z-1]for j in [X-1,Y,Z-1]for k in [X-1,Y-1,Z] if 0<= i <= X if 0<= j<=Y if 0<=k<=Z if (i+j+k)!=N]
-    combinaciones.append((0,0,0))
-    print(combinaciones)
-    combinacionSinRepetir=[]
-    for i in combinaciones:
-        if list(i) not in combinacionSinRepetir:
-            combinacionSinRepetir.append(list(i))
-            combinacionSinRepetir.sort()
-    print(combinacionSinRepetir)
+    print([[i,j,k] for i in range(X+1) for j in range(Y+1) for k in range(Z+1) if i+j+k != N])

520100a4b8a49b9309a9b5bf03a1924e55b4edf1
MauricioAcosta <andres.taamap@gmail.com>
2017-04-26 18:18:53 -0500

    Set .symmetric_difference() Operation

---
+++ Python/operation_symmetric_difference.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    num_first=int(input())
+    set_first=set(list(map(int, input().split()))[:num_first])
+    num_second=int(input())
+    set_second=set(list(map(int, input().split()))[:num_second])
+    print(len(set_first ^ set_second))

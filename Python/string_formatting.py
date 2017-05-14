cbd92545553e62efc34d9167f608be9a03d678b2
MauricioAcosta <andres.taamap@gmail.com>
2017-05-13 20:32:21 -0500

    agregado metodo

--- Python/string_formatting.py
+++ Python/string_formatting.py
@@ -1,4 +1,5 @@
-n = int(raw_input())
-width = len("{0:b}".format(n))
-for i in xrange(1,n+1):
-  print "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width)
+def print_formatted(number):
+    width = len("{0:b}".format(number))
+    for i in range(1,number+1):
+        print "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width)
+print_formatted(input())

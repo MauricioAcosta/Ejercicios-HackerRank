84d35d557127e99e470bb130ebcc6dd3f7ad0965
MauricioAcosta <andres.taamap@gmail.com>
2017-05-13 20:35:49 -0500

    Mejorado el tiempo de compilación

--- Python/string_formatting.py
+++ Python/string_formatting.py
@@ -1,5 +1,4 @@
-def print_formatted(number):
-    width = len("{0:b}".format(number))
-    for i in range(1,number+1):
-        print "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width)
-print_formatted(input())
+n = int(input())
+width = len("{0:b}".format(n))
+for i in range(1,n+1):
+  print ("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
